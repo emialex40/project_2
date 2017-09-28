5820df35b49ef67d97b13a2c1273e5ad8a544718
emialex40 <26emic73@gmail.com>
2017-09-28 09:52:26 +0300

    27.09.2017 v1.0

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
-	//custom JS
+	$('.btn-white').hover(
+		function() {
+			$('.s-video__btn-line').css({
+				"right": "-15px",
+				"background": "#FFB30C",
+				"transition": "1s"
+			});
+			$('.btn-white').css("color", "#FFB30C");
+		},
+
+		function() {
+			$('.s-video__btn-line').css({
+				"right": "0",
+				"background": "#fe9200",
+				"transition": "1s"
+			});
+			$('.btn-white').css("color", "#fe9200");
+		});
 
 });
